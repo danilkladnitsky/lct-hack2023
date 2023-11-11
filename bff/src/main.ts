6e56c7c47e1fb5f4c11739b4e1ce030eeb80f3b8
Danil Kladnitsky <danyakladnitsky@gmail.com>
2023-11-11 17:29:42 +0300

    add ssl

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -3,8 +3,19 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 
 import { AppModule } from './app.module';
 
+import fullChain from './certificate/fullchain.pem';
+import privkey from './certificate/privkey.pem';
+
+const httpsOptions = {
+  cert: fullChain,
+  key: privkey,
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions,
+  });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(3000, '0.0.0.0');
 }
