db0a0a222d67e5a799a407870021f129993869ea
Danil Kladnitsky <danyakladnitsky@gmail.com>
2023-11-11 16:51:42 +0300

    fix url

--- frontend/src/socket/index.ts
+++ frontend/src/socket/index.ts
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/stream';
+const URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://lct.1431207-ck39036.tw1.ru:3000/stream'
+    : 'http://localhost:3000/stream';
 
 export const socket = io(URL, {
   autoConnect: true,
